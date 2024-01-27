ee5d61d686fa5fd5c7c85fa59cc28fa9c15681ab
KamilArturZajac <157361644+KamilArturZajac@users.noreply.github.com>
2024-01-27 14:26:13 +0100

    Update StudentTest.java

--- StudentTest.java
+++ StudentTest.java
@@ -9,27 +9,25 @@ import java.time.LocalDate;
 
 public class StudentTest {
     private final UniversityManager universityManager = new UniversityManager();
+    private Student student; // instancja klasy Student
 
-@BeforeEach
-void createStudent() {
-    // Utwórz obiekt Student i przypisz go do zmiennej student
-    Student student = new Student("testId", "firstName", "lastName", LocalDate.of(1995, 5, 5));
-}
+    @BeforeEach
+    void createStudent() {
+        student = new Student("studentId", "firstName", "lastName", LocalDate.of(1995, 5, 5));
+    }
 
     @Test
     @DisplayName("Sprawdź imię studenta")
     void getFirstName() {
-        // given - utworzony student
         // when
         String firstName = student.getFirstName();
         // then
         Assertions.assertEquals("firstName", firstName);
     }
-    
+
     @Test
     @DisplayName("Sprawdź nazwisko studenta")
     void getLastName() {
-        // given - utworzony student
         // when
         String lastName = student.getLastName();
         // then
