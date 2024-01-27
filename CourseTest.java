605f7a9088d80e09ac763409fa54c6904beb3ab3
KamilArturZajac <157361644+KamilArturZajac@users.noreply.github.com>
2024-01-27 15:00:40 +0100

    Update CourseTest.java

--- CourseTest.java
+++ CourseTest.java
@@ -5,37 +5,37 @@ import org.junit.jupiter.api.Test;
 import pl.wsb.university.Class.Course;
 import pl.wsb.university.Class.Semester;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CourseTest {
     private Course course;
+    private List<String> members;
 
     @BeforeEach
     void createCourse() {
-        List<String> members = new ArrayList<>();
+        members = new ArrayList<>();
         members.add("Student1");
         members.add("Student2");
         members.add("Student3");
         members.add("Student4");
         members.add("Student5");
-        course = new Course("courseId", "name", 1, Semester.SUMMER, "leader", (ArrayList) members);
+
+        course = new Course("courseId", "name", 1, Semester.SUMMER, "leader", new ArrayList<>(members));
     }
 
     @Test
     @DisplayName("Sprawdź ID kursu")
     void getId() {
-        //given - obiekt kurs z testowymi parametrami
         // when
         String ID = course.getCourseId();
         // then
         Assertions.assertEquals("courseId", ID);
     }
+
     @Test
     @DisplayName("Sprawdź nazwę kursu")
     void getName() {
-        //given - obiekt kurs z testowymi parametrami
         // when
         String name = course.getName();
         // then
@@ -45,7 +45,6 @@ public class CourseTest {
     @Test
     @DisplayName("Sprawdź na którym roku jest kurs")
     void getYear() {
-        //given - obiekt kurs z testowymi parametrami
         // when
         int year = course.getYear();
         // then
@@ -54,8 +53,7 @@ public class CourseTest {
 
     @Test
     @DisplayName("Sprawdź semestr kursu")
-    void getSemestr() {
-        //given - obiekt kurs z testowymi parametrami
+    void getSemester() {
         // when
         Semester semester = course.getSemester();
         // then
@@ -65,7 +63,6 @@ public class CourseTest {
     @Test
     @DisplayName("Sprawdź listę uczestników kursu")
     void getMembers() {
-        // given - obiekt kurs z testowymi parametrami
         // when
         List<String> testMembers = course.getMembers();
         // then
