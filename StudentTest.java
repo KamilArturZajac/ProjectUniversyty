9122f8a6a63853b38bb572fc0b8d242842b9a872
KamilArturZajac <157361644+KamilArturZajac@users.noreply.github.com>
2024-01-27 14:23:44 +0100

    Update StudentTest.java

--- StudentTest.java
+++ StudentTest.java
@@ -10,30 +10,28 @@ import java.time.LocalDate;
 public class StudentTest {
     private final UniversityManager universityManager = new UniversityManager();
 
-    @BeforeEach
-    void createStudent() {
-        String studentTest1 = universityManager.recruitNewStudent("firstName", "lastName", LocalDate.of(1995, 5, 5));
-        String studentTest2 = universityManager.recruitNewStudent("firstName", "lastName", LocalDate.of(1888, 5, 5));
-        String studentTest3 = universityManager.recruitNewStudent("firstName", "lastName", LocalDate.of(2100, 5, 5));
-        String studentTest4 = universityManager.recruitNewStudent("firstName", "lastName", LocalDate.of(2000, 15, 5));
-        String studentTest5 = universityManager.recruitNewStudent("firstName", "lastName", LocalDate.of(2000, 5, 45));
-    }
+@BeforeEach
+void createStudent() {
+    // Utwórz obiekt Student i przypisz go do zmiennej student
+    Student student = new Student("testId", "firstName", "lastName", LocalDate.of(1995, 5, 5));
+}
 
     @Test
     @DisplayName("Sprawdź imię studenta")
     void getFirstName() {
-        // given - kilka studentów
+        // given - utworzony student
         // when
-        String firstName = Student.getFirstName();
+        String firstName = student.getFirstName();
         // then
         Assertions.assertEquals("firstName", firstName);
     }
+    
     @Test
     @DisplayName("Sprawdź nazwisko studenta")
     void getLastName() {
-        // given - kilka studentów
+        // given - utworzony student
         // when
-        String lastName = Student.getLastName();
+        String lastName = student.getLastName();
         // then
         Assertions.assertEquals("lastName", lastName);
     }
